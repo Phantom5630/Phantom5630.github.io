2bd591984ecec9794d6fca389ac09b7e6e517e32
Phantom5630 <pinkherr5630@gmail.com>
2024-09-14 14:16:43 +0800

    rename "My Portfolio" folder to "my-portfolio"

--- my-portfolio/scripts/typeWriterEffect.js
+++ my-portfolio/scripts/typeWriterEffect.js
@@ -1,32 +1,35 @@
 const text = "A game programmer,\ncurrently studying in UOW Malaysia KDU.";
-let index = 0;
+let index1 = 0;
 let startCorrection = false;
 
 function typewriter() {
 	if (!startCorrection) {
-		if (index < text.length) {
-			const char = text.charAt(index);
+		if (index1 < text.length) {
+			const char = text.charAt(index1);
 			if (char == "\n") {
 				document.getElementById("typewriter").innerHTML += "<br>";
 			}
 			else {
 				document.getElementById("typewriter").innerHTML += char;
 			}
-			index++;
-			if (index == text.length) {
+			index1++;
+			if (index1 == text.length) {
 				startCorrection = true;
 			}
 			if (char == " " || char == "\n") {
 				setTimeout(typewriter, 100);
+			}
+			else if (char == "."){
+				setTimeout(typewriter, 200);
 			}
 			else {
 				setTimeout(typewriter, Math.random() * 20 + 40);
 			}
 		}
 	}
-	else if (index >= text.length - 4) {
-		index--;
-		document.getElementById("typewriter").innerHTML = document.getElementById("typewriter").innerHTML.substring(0, index + 3);
+	else if (index1 >= text.length - 4) {
+		index1--;
+		document.getElementById("typewriter").innerHTML = document.getElementById("typewriter").innerHTML.substring(0, index1 + 3);
 		setTimeout(typewriter, 40);
 	}
 	else {
